daf61d55efa73d1ca8499ea733e7e1de2925281a
Hardik Rathod <hardikrathod1219@gmail.com>
2024-11-25 13:27:34 +0530

    feat(CTA section): animations added

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -1,33 +1,57 @@
 import { Circle } from "@/components/Circle";
 import { CutCornerButton } from "@/components/CutCornerButton";
 import { Hexagon } from "@/components/Hexagon";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
 
 export const CallToAction = () => {
+    const sectionRef = useRef(null);
+
+    const { scrollYProgress } = useScroll({
+        target: sectionRef,
+        offset: ["start end", "end start"],
+    });
+
+    const rotate = useTransform(scrollYProgress, [0, 1], [45, -45]);
+
     return (
-        <section className="overflow-hidden py-60">
+        <section className="overflow-hidden py-60" ref={sectionRef}>
             <div className="container">
                 <div className="relative">
                     <div className="left absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-                        <Hexagon className="size-[700px]" />
+                        <Hexagon className="size-[700px]" size={700} />
                     </div>
                     <div className="left absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-                        <Hexagon className="size-[1100px]" />
+                        <Hexagon
+                            className="size-[1100px]"
+                            size={1100}
+                            reverse
+                            duration={35}
+                        />
                     </div>
                     <div className="left absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-                        <Circle className="absolute -top-[400px] left-[0]">
-                            <img
+                        <Circle
+                            className="absolute -top-[400px] left-[0]"
+                            animate
+                        >
+                            <motion.img
                                 src="/public/assets/images/cuboid.png"
                                 alt="Cuboid 3D image"
                                 className="size-36"
+                                style={{ rotate }}
                             />
                         </Circle>
                     </div>
                     <div className="left absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-                        <Circle className="absolute -left-[600px] -top-[70px]">
-                            <img
+                        <Circle
+                            className="absolute -left-[600px] -top-[70px]"
+                            animate
+                        >
+                            <motion.img
                                 src="/public/assets/images/cylinder.png"
                                 alt="Cylinder 3D image"
                                 className="size-36"
+                                style={{ rotate }}
                             />
                         </Circle>
                     </div>
