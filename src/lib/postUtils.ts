7d7a1961658c6ee54cc4b4bb8e5106804228ff86
Hardik Rathod <hardikrathod1219@gmail.com>
2024-11-25 18:03:21 +0530

    feat(Careers page): All screens and animations done

--- src/lib/postUtils.ts
+++ src/lib/postUtils.ts
@@ -17,3 +17,35 @@ export const getPostColorFromCategory = (
             return "fuchsia";
     }
 };
+
+export const getColorFromPositionType = (
+    type: CollectionEntry<"positions">["data"]["type"],
+): TColors => {
+    switch (type) {
+        case "Full Time":
+            return "lime";
+        case "Part Time":
+            return "cyan";
+        case "Internship":
+            return "cyan";
+        case "Contract":
+            return "violet";
+        default:
+            return "fuchsia";
+    }
+};
+
+export const getColorFromPositionCategory = (
+    type: CollectionEntry<"positions">["data"]["category"],
+): TColors => {
+    switch (type) {
+        case "Engineering":
+            return "lime";
+        case "Product":
+            return "cyan";
+        case "Marketing":
+            return "violet";
+        default:
+            return "fuchsia";
+    }
+};
